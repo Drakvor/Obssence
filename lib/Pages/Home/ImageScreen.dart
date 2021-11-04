f3bc97786fbdc9ddf1d2f6c5f8f316d020e796de
Matt Kershaw <matt@kershaw.org>
2021-11-04 16:52:47 +0000

    Attempted implementation of image zoom. Currently untested.

--- lib/Pages/Home/ImageScreen.dart
+++ lib/Pages/Home/ImageScreen.dart
@@ -13,15 +13,34 @@ class ImageScreen extends StatefulWidget {
 
 class _ImageScreenState extends State<ImageScreen> {
   final File image;
+  double _baseScaleFactor = 1.0;
+  double _scaleFactor = 1.0;
   _ImageScreenState(this.image);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          Image.file(image),
-        ],
+    return GestureDetector(
+      onScaleStart: (ScaleStartDetails scaleStartDetails) {
+        _baseScaleFactor = _scaleFactor;
+      },
+      onScaleUpdate: (ScaleUpdateDetails scaleUpdateDetails) {
+        if (scaleUpdateDetails.scale == 1.0){
+          return;
+        }
+        setState((){
+          _scaleFactor = _baseScaleFactor * scaleUpdateDetails.scale;
+        });
+      },
+      child: ClipRect(
+        child: Align(
+          widthFactor: 1.0,
+          heightFactor: 1.0,
+          child: Image.file(
+              image,
+              width: MediaQuery.of(context).size.width * _scaleFactor,
+              height: MediaQuery.of(context).size.height * _scaleFactor,
+          ),
+        ),
       ),
     );
   }
