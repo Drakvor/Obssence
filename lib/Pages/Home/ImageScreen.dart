77631c6f24de5e4aa76a6c92c930907523504976
Matt Kershaw <matt@kershaw.org>
2021-11-05 10:14:02 +0000

    Imported photo_view.dart, implemented, tested.

--- lib/Pages/Home/ImageScreen.dart
+++ lib/Pages/Home/ImageScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
+import 'package:photo_view/photo_view.dart';
 import 'package:luxury_app_pre/Management/Utils.dart';
 
 class ImageScreen extends StatefulWidget {
@@ -13,35 +14,17 @@ class ImageScreen extends StatefulWidget {
 
 class _ImageScreenState extends State<ImageScreen> {
   final File image;
-  double _baseScaleFactor = 1.0;
-  double _scaleFactor = 1.0;
   _ImageScreenState(this.image);
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onScaleStart: (ScaleStartDetails scaleStartDetails) {
-        _baseScaleFactor = _scaleFactor;
-      },
-      onScaleUpdate: (ScaleUpdateDetails scaleUpdateDetails) {
-        if (scaleUpdateDetails.scale == 1.0){
-          return;
-        }
-        setState((){
-          _scaleFactor = _baseScaleFactor * scaleUpdateDetails.scale;
-        });
-      },
-      child: ClipRect(
-        child: Align(
-          widthFactor: 1.0,
-          heightFactor: 1.0,
-          child: Image.file(
-              image,
-              width: MediaQuery.of(context).size.width * _scaleFactor,
-              height: MediaQuery.of(context).size.height * _scaleFactor,
-          ),
+    return Container(
+      child: PhotoView(
+        backgroundDecoration: new BoxDecoration(
+            color: utils.resourceManager.colours.background,
         ),
-      ),
+        imageProvider: Image.file(image,).image,
+      )
     );
   }
 }
